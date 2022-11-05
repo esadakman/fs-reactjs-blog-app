c48d2b5a996c0549181b641e8983cf7fbea70bc8
esadakman <esadd26@gmail.com>
2022-11-05 16:56:57 +0300

    Post Card design

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,11 +1,78 @@
 import React from "react";
-import PostDetails from "./PostDetails";
+// import PostDetails from "./PostDetails";
 
 const PostCard = () => {
   return (
-     <>
-     <PostDetails/>
-     </>
+    <>
+      {/* <PostDetails/> */}
+      <div className="min-h-screen bg-gray-100 flex justify-center items-center text-white  ">
+        <div className="max-w-xs container bg-white rounded-xl shadow-lg transform hover:scale-105 hover:shadow-2xl dark:bg-slate-800  transition-all">
+          <div>
+            <span className="text-white text-xs font-bold rounded-lg bg-green-500 inline-block mt-4 ml-4 py-1.5 px-4 cursor-pointer">
+              Home
+            </span>
+            <h1 className="text-2xl my-2 ml-4 font-bold hover:text-slate-200 cursor-pointer  transition duration-100">
+              Lampara Look
+            </h1> 
+          </div>
+          <img
+            className="w-full cursor-pointer"
+            src="https://images.unsplash.com/photo-1525268771113-32d9e9021a97?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+            alt=""
+          />
+          <div className="flex p-4 justify-between">
+            <div className="flex items-center space-x-2">
+              <img
+                className="w-10 rounded-full"
+                src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+                alt="sara"
+              />
+              <h2 className=" font-bold cursor-pointer">
+                Felipe Sacudon
+              </h2>
+            </div>
+            <div className="flex space-x-2">
+              <div className="flex space-x-1 items-center">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7 transition-all hover:text-slate-300  cursor-pointer"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                    />
+                  </svg>
+                </span>
+                <span>22</span>
+              </div>
+              <div className="flex space-x-1 items-center">
+                <span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7 text-red-500 hover:text-red-400 transition-all cursor-pointer"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <span>20</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
